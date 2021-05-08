7b07f3aff6450491d2c31c2a68a2b505fc653af6
Dmytro Khylko <dmytro.khylko@gmail.com>
2021-05-08 16:20:38 +0300

    change main page name

--- lab10/src/app.py
+++ lab10/src/app.py
@@ -5,7 +5,7 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route("/")
 def home():
-    return render_template("greeting.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
